3f81a7f1ecd2c12da42ac8291784a5810c5c025b
Krzysztof Sala <pompadom@wp.pl>
2022-02-06 21:40:35 +0100

    Add files via upload

---
+++ main/DTSU666H_translator.cpp
@@ -0,0 +1,213 @@
+// =================================================================================================
+// eModbus: Copyright 2020 by Michael Harwerth, Bert Melis and the contributors to ModbusClient
+//               MIT license - see license.md for details
+// =================================================================================================
+/* Example code for exchange data between DTSU666 CHINT and DTSU666H Huawei Power Meter
+using eMOdbus library from https://github.com/eModbus
+when you activate power meter in smartfon aplication the SUN2000 start sending request "0B 03 07 D1 00 01 D5 ED"  where
+0B - Power Meter Modbus address (default 11)
+03 - Function (READ_HOLD_REGISTER)
+07 D1 - register address to read (dec 2001 )
+00 01 - how many registers read (1 register mean 2 bytes)
+D5 ED - CRC checksum
+
+If register address 0x07D1 = 0x3F80 the sun2000 recognize it as  PowerMeter is active
+slave answer should look like this  "0B 03 02 3F 80 30 15" 
+after this SUN200 send request  "0B 03 08 36 00 50 A7 32"
+and we should send value 80  registers begnining from address 0x0836  (dec 2002)
+from time to time SUN2000 send request 0B 03 08 A6 00 0A 27 24 but i do not know what exactly is stored in this 10 registers begining from 0x08A6. I asked support Huawai but they have not answered.
+Information about HUAWEI registers addresses
+https://forum.huawei.com/enterprise/en/how-to-connect-custom-meter-dtsu666-h-to-smartlogger1000/thread/599712-100027
+Information about CHINT registers addresses
+https://www.aggsoft.com/serial-data-logger/tutorials/modbus-data-logging/chint-instrument-dsu666-dtsu666.htm
+I divided request to CHINT into two part because for one request the number of  registers in answer could be to large
+*/ 
+
+
+#include "HardwareSerial.h"  // from the <Arduino.h>  librrary  for Serial, millis()  etc.
+#include "Logging.h"
+#include "ModbusClientRTU.h"	// header for the Modbus Client RTU style from eModbus Library
+#include "ModbusServerRTU.h"
+// Definitions for this special case
+#define RX1_PIN GPIO_NUM_18
+#define TX1_PIN GPIO_NUM_19
+#define REDE1_PIN GPIO_NUM_23
+#define RX2_PIN GPIO_NUM_16
+#define TX2_PIN GPIO_NUM_17
+#define REDE2_PIN GPIO_NUM_15
+#define HUAWEI_ID 0x0B // must be this same address as in setup Huawey inverter
+#define CHINT_ID 0x01	// must be this same address as in setup CHINT Power Meter
+#define BAUDRATE 9600
+#define HUAWEI_START_REG_1 0x0836 //SUN2000 ask for 80 registers beginning from this address	
+#define HUAWEI_START_REG_2 0x08A6 //SUN2000 ask for 10 registers but i don't know for what. Huawei did nor answer my question.
+#define CHINT_START_REG_1 0x2000  //
+#define CHINT_START_REG_2 0x401E
+#define CHINT_REQUEST1 82
+#define CHINT_REQUEST2 60
+#define READ_INTERVAL 2000  // setup how often read the CHINT Power Meter
+bool data_ready = false;
+int errors = 1;  //moje;
+char IsPowerMeterActive1= char(0x3F);
+char IsPowerMeterActive2= char(0x80);
+uint32_t request_time;
+uint16_t Chint_RegData[CHINT_REQUEST1+CHINT_REQUEST2+10];  // for read from CHINT  to do zmiany
+
+/*
+
+int HuaweiTranslate[CHINT_REQUEST1+CHINT_REQUEST2+50]={
+ 0, 1, 2, 3, 4, 5, 6, 7, 
+ 8, 9,10,11,12,13,14,15,
+16,17,18,19,20,21,22,23,
+24,25,26,27,28,29,30,31,
+32,33,34,35,36,37,38,39,
+40,41,42,43,44,45,46,47,
+48,49,50,51,52,53,54,55,
+56,57,58,59,60,61,62,63,
+64,65,66,67,68,69,
+70,71,72,73,74,75,76,77,78,79,
+80,81,82,83,84
+};
+int HuaweiTranslate[CHINT_REQUEST1+CHINT_REQUEST2+50]={
+ 12, 13, 14, 15, 16, 17, 6, 7,  2111
+ 8, 9,10,11,x16,x17,0,1,2,3     2121
+4,5,x24,x25,18,19,20,21,22,23,   2131
+24,25,26,27,28,29,30,31,32,33,   2141
+34,35,36,37,38,39,40,41,42,43,   2151
+44,45,46,47,48,49,50,51,52,53,   2161
+54,55,56,57,86,87,60,61,62,63,    2171
+64,65,88,89,68,69,70,71,72,73,    2181
+74,75,
+72,73,74,75,76,77,78,79,
+80,81,82,83
+};
+*/
+
+int HuaweiTranslate[CHINT_REQUEST1+CHINT_REQUEST2+50]=
+{12,13,14,15,16,17,6,7,
+8,9,10,11,80,81,0,1,2,3,
+4,5,80,81,18,19,20,21,22,23,
+24,25,26,27,28,29,30,31,32,33,
+34,35,36,37,38,39,40,41,42,43,
+44,45,46,47,48,49,50,51,52,53,
+54,55,56,57,86,87,60,61,62,63,
+64,65,88,89,68,69,70,71,72,73,
+74,75,
+72,73,74,75,76,77,78,79,
+80,81,82,83
+};
+
+// The RS485 module has no halfduplex,so the second parameter with the DE/RE pin is required! https://emodbus.github.io/modbusserver-rtu-api
+//	Create a ModbusRTU Server Slave instance listening on Serial2 with 20000ms timeout RE/DE control pin GPIO_NUM_15  
+ModbusServerRTU MBserver(Serial2, 2000, REDE2_PIN);  
+//	Create a Modbus RTE Master Client RE/DE control pin GPIO_NUM_21
+ModbusClientRTU MbChint(Serial1, REDE1_PIN); 	
+
+// FC03: worker to create answer  Modbus function code 0x03 (READ_HOLD_REGISTER) for Huawei Master Client
+ModbusMessage FC03(ModbusMessage request) {
+	uint16_t words; 
+	uint16_t address;
+	uint16_t* HuReg;
+	HuReg=&Chint_RegData[0];
+	ModbusMessage response;  	// declare response message to be sent back to Huawei
+	request.get(2, address);
+	request.get(4, words);
+	response.add(request.getServerID(), request.getFunctionCode(), (uint8_t)(words*2));	//Set up message with serverID, FC and length of data (1 register is 2 byte)
+	
+	// on request for 0x2001 register  "0B 03 07 D1 00 01 D5 ED"  answer 0x3F80 means that Power Meter is active 
+	if ((address == 0x0835)&& (words==0x01))  response.add(uint16_t(0x3F80)); 
+	else if ((address==HUAWEI_START_REG_1)&& (words>0x0A)) {       // read data part 1 request 0B 03 08 36 00 50 A7 32
+		for (uint16_t i = 0; i < words; i++) {
+			response.add(*(HuReg+HuaweiTranslate[i]));
+		}}
+	else if ((address == HUAWEI_START_REG_2)&& (words==0x0A)) {   //  read data part 2 request 0B 03 08 A6 00 0A 27 24 
+		for (uint16_t i = 80; i < (i+ words); i++) {
+			response.add(0x0000);  // answer for 10 unknown registers is set to zero
+		}}
+	else {  // No, either address or words are outside the limits. Set up error response.
+    response.setError(request.getServerID(), request.getFunctionCode(), ILLEGAL_DATA_ADDRESS);
+	Serial.printf("ERROR bad address= %X \n", address);
+	}
+	return response;
+}
+
+// Define an onData handler function to receive the regular responses
+// Arguments are received response message and the request's token
+// The device has values all as IEEE754 float32 in two consecutive registers
+// Read the CHint response in a loop
+void handleData1(ModbusMessage response, uint32_t token) {
+  uint8_t words;      			// response CHINT number of registers
+  response.get(2, words);
+  uint16_t offs = 3;   // First value is on pos 3, after server ID, function code and length byte
+	if (words==2*CHINT_REQUEST1) {  // is this answer for part1?
+	  for (uint8_t i = 0; i < words/2; i++) {
+		offs = response.get(offs, Chint_RegData[i]);
+	}}
+	else if (words==2*CHINT_REQUEST2) {
+		 for (uint8_t i = 0; i < words/2; i++) {
+		offs = response.get(offs, Chint_RegData[i+CHINT_REQUEST1]);  // is this answer for part1?
+	}}
+	else{
+	Serial.printf("error read from");
+	}
+  // Signal "data is complete"
+  request_time = token;
+  data_ready = true;
+}
+
+// Define an onError handler function to receive error responses
+// Arguments are the error code returned and a user-supplied token to identify the causing request
+void handleError(Error error, uint32_t token) {
+  // ModbusError wraps the error code and provides a readable error message for it
+  ModbusError me(error);
+  Serial.printf("\nError me number  %u  token %u \n", errors++, token);
+  LOG_E("Error response: %02X - %s\n", (int)me, (const char *)me);
+}
+
+// Setup() - initialization happens here
+void setup() {
+	Serial1.setRxBufferSize(512);  // default the buffer size is 256
+	Serial.begin(115200);  // Init Serial port monitor for development
+	while (!Serial) {}
+	Serial.print("Serial0 OK __  ");
+	
+	Serial1.begin(9600, SERIAL_8N2, RX1_PIN, TX1_PIN); //Serial1 connected to Modbus RTU CHINT  
+	while (!Serial1) {}
+	Serial.print("Serial1 OK __  ");
+
+    MbChint.onDataHandler(&handleData1); 	// Set up ModbusRTU client - provide onData handler function	
+    MbChint.onErrorHandler(&handleError);	// - provide onError handler function
+    MbChint.setTimeout(2000);			// Set message timeout to 2000ms
+    MbChint.begin(0);				// Start ModbusRTU background task
+
+	Serial2.begin(9600, SERIAL_8N2, RX2_PIN, TX2_PIN);  //Serial2 connected to Modbus RTU SUN2000
+	while (!Serial2) {}
+	Serial.println("Serial2 OK __");
+	MBserver.registerWorker(0x0B, READ_HOLD_REGISTER, &FC03 ); // Register served function code worker for server 11, FC 0x03
+	MBserver.start(1); // Start ModbusRTU background task
+}
+
+// loop() - cyclically request the data from CHINT
+void loop() {
+  static uint32_t next_request = millis();
+  if (millis() - next_request > READ_INTERVAL) {
+    data_ready = false;
+    Error err = MbChint.addRequest(millis(),CHINT_ID, READ_HOLD_REGISTER, CHINT_START_REG_1, CHINT_REQUEST1);
+    if (err==SUCCESS) {
+		delay(10);    				// delay to get different token ID for request for part2
+		err = MbChint.addRequest(millis(),CHINT_ID, READ_HOLD_REGISTER, CHINT_START_REG_2, CHINT_REQUEST2);
+	}
+    if (err!=SUCCESS) {
+		ModbusError e(err);
+		LOG_E("Error creating request: %02X - %s\n", (int)e, (const char *)e);
+    }
+    next_request = millis();    // Save current time to check for next cycle
+	} else {
+		if (data_ready) {
+		  data_ready = false;		
+		Serial.printf("Loop end time %ld  (ms)\n", millis());
+// We can print on monitor port the data received from CHINT
+//for (uint8_t j = 0; j < (CHINT_REQUEST1); j++)Serial.printf("%i; %x: ", (j+ CHINT_START_REG_1), Chint_RegData[j]);
+//for (uint8_t j=0;j<(CHINT_REQUEST2); j++) Serial.printf("%i: %x:",(j+CHINT_START_REG_2),Chint_RegData[CHINT_REQUEST1+j]);     
+		}
+	}
+}
---
+++ main/RTUutils.cpp
@@ -0,0 +1,295 @@
+// =================================================================================================
+// eModbus: Copyright 2020 by Michael Harwerth, Bert Melis and the contributors to eModbus
+//               MIT license - see license.md for details
+// =================================================================================================
+#include "options.h"
+#include "ModbusMessage.h"
+#include "RTUutils.h"
+#undef LOCAL_LOG_LEVEL
+// #define LOCAL_LOG_LEVEL LOG_LEVEL_VERBOSE
+#include "Logging.h"
+
+// calcCRC: calculate Modbus CRC16 on a given array of bytes
+uint16_t RTUutils::calcCRC(const uint8_t *data, uint16_t len) {
+  // CRC16 pre-calculated tables
+  const uint8_t crcHiTable[] = {
+    0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41, 0x01, 0xC0, 0x80, 0x41, 0x00, 0xC1, 0x81,
+    0x40, 0x01, 0xC0, 0x80, 0x41, 0x00, 0xC1, 0x81, 0x40, 0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0,
+    0x80, 0x41, 0x01, 0xC0, 0x80, 0x41, 0x00, 0xC1, 0x81, 0x40, 0x00, 0xC1, 0x81, 0x40, 0x01,
+    0xC0, 0x80, 0x41, 0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41, 0x01, 0xC0, 0x80, 0x41,
+    0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41, 0x00, 0xC1, 0x81, 0x40, 0x00, 0xC1, 0x81,
+    0x40, 0x01, 0xC0, 0x80, 0x41, 0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41, 0x01, 0xC0,
+    0x80, 0x41, 0x00, 0xC1, 0x81, 0x40, 0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41, 0x01,
+    0xC0, 0x80, 0x41, 0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41, 0x00, 0xC1, 0x81, 0x40,
+    0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41, 0x01, 0xC0, 0x80, 0x41, 0x00, 0xC1, 0x81,
+    0x40, 0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41, 0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0,
+    0x80, 0x41, 0x01, 0xC0, 0x80, 0x41, 0x00, 0xC1, 0x81, 0x40, 0x00, 0xC1, 0x81, 0x40, 0x01,
+    0xC0, 0x80, 0x41, 0x01, 0xC0, 0x80, 0x41, 0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41,
+    0x00, 0xC1, 0x81, 0x40, 0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41, 0x00, 0xC1, 0x81,
+    0x40, 0x01, 0xC0, 0x80, 0x41, 0x01, 0xC0, 0x80, 0x41, 0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0,
+    0x80, 0x41, 0x00, 0xC1, 0x81, 0x40, 0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41, 0x01,
+    0xC0, 0x80, 0x41, 0x00, 0xC1, 0x81, 0x40, 0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41,
+    0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41, 0x01, 0xC0, 0x80, 0x41, 0x00, 0xC1, 0x81,
+    0x40
+  };
+
+  const uint8_t crcLoTable[] = {
+    0x00, 0xC0, 0xC1, 0x01, 0xC3, 0x03, 0x02, 0xC2, 0xC6, 0x06, 0x07, 0xC7, 0x05, 0xC5, 0xC4,
+    0x04, 0xCC, 0x0C, 0x0D, 0xCD, 0x0F, 0xCF, 0xCE, 0x0E, 0x0A, 0xCA, 0xCB, 0x0B, 0xC9, 0x09,
+    0x08, 0xC8, 0xD8, 0x18, 0x19, 0xD9, 0x1B, 0xDB, 0xDA, 0x1A, 0x1E, 0xDE, 0xDF, 0x1F, 0xDD,
+    0x1D, 0x1C, 0xDC, 0x14, 0xD4, 0xD5, 0x15, 0xD7, 0x17, 0x16, 0xD6, 0xD2, 0x12, 0x13, 0xD3,
+    0x11, 0xD1, 0xD0, 0x10, 0xF0, 0x30, 0x31, 0xF1, 0x33, 0xF3, 0xF2, 0x32, 0x36, 0xF6, 0xF7,
+    0x37, 0xF5, 0x35, 0x34, 0xF4, 0x3C, 0xFC, 0xFD, 0x3D, 0xFF, 0x3F, 0x3E, 0xFE, 0xFA, 0x3A,
+    0x3B, 0xFB, 0x39, 0xF9, 0xF8, 0x38, 0x28, 0xE8, 0xE9, 0x29, 0xEB, 0x2B, 0x2A, 0xEA, 0xEE,
+    0x2E, 0x2F, 0xEF, 0x2D, 0xED, 0xEC, 0x2C, 0xE4, 0x24, 0x25, 0xE5, 0x27, 0xE7, 0xE6, 0x26,
+    0x22, 0xE2, 0xE3, 0x23, 0xE1, 0x21, 0x20, 0xE0, 0xA0, 0x60, 0x61, 0xA1, 0x63, 0xA3, 0xA2,
+    0x62, 0x66, 0xA6, 0xA7, 0x67, 0xA5, 0x65, 0x64, 0xA4, 0x6C, 0xAC, 0xAD, 0x6D, 0xAF, 0x6F,
+    0x6E, 0xAE, 0xAA, 0x6A, 0x6B, 0xAB, 0x69, 0xA9, 0xA8, 0x68, 0x78, 0xB8, 0xB9, 0x79, 0xBB,
+    0x7B, 0x7A, 0xBA, 0xBE, 0x7E, 0x7F, 0xBF, 0x7D, 0xBD, 0xBC, 0x7C, 0xB4, 0x74, 0x75, 0xB5,
+    0x77, 0xB7, 0xB6, 0x76, 0x72, 0xB2, 0xB3, 0x73, 0xB1, 0x71, 0x70, 0xB0, 0x50, 0x90, 0x91,
+    0x51, 0x93, 0x53, 0x52, 0x92, 0x96, 0x56, 0x57, 0x97, 0x55, 0x95, 0x94, 0x54, 0x9C, 0x5C,
+    0x5D, 0x9D, 0x5F, 0x9F, 0x9E, 0x5E, 0x5A, 0x9A, 0x9B, 0x5B, 0x99, 0x59, 0x58, 0x98, 0x88,
+    0x48, 0x49, 0x89, 0x4B, 0x8B, 0x8A, 0x4A, 0x4E, 0x8E, 0x8F, 0x4F, 0x8D, 0x4D, 0x4C, 0x8C,
+    0x44, 0x84, 0x85, 0x45, 0x87, 0x47, 0x46, 0x86, 0x82, 0x42, 0x43, 0x83, 0x41, 0x81, 0x80,
+    0x40
+  };
+
+  register uint8_t crcHi = 0xFF;
+  register uint8_t crcLo = 0xFF;
+  register uint8_t index;
+
+  while (len--) {
+    index = crcLo ^ *data++;
+    crcLo = crcHi ^ crcHiTable[index];
+    crcHi = crcLoTable[index];
+  }
+  return (crcHi << 8 | crcLo);
+}
+
+// calcCRC: calculate Modbus CRC16 on a given message
+uint16_t RTUutils::calcCRC(ModbusMessage msg) {
+  return calcCRC(msg.data(), msg.size());
+}
+
+
+// validCRC #1: check the given CRC in a block of data for correctness
+bool RTUutils::validCRC(const uint8_t *data, uint16_t len) {
+  return validCRC(data, len - 2, data[len - 2] | (data[len - 1] << 8));
+}
+
+// validCRC #2: check the CRC of a block of data against a given one for equality
+bool RTUutils::validCRC(const uint8_t *data, uint16_t len, uint16_t CRC) {
+  uint16_t crc16 = calcCRC(data, len);
+  if (CRC == crc16) return true;
+  return false;
+}
+
+// validCRC #3: check the given CRC in a message for correctness
+bool RTUutils::validCRC(ModbusMessage msg) {
+  return validCRC(msg.data(), msg.size() - 2, msg[msg.size() - 2] | (msg[msg.size() - 1] << 8));
+}
+
+// validCRC #4: check the CRC of a message against a given one for equality
+bool RTUutils::validCRC(ModbusMessage msg, uint16_t CRC) {
+  return validCRC(msg.data(), msg.size(), CRC);
+}
+
+// addCRC: calculate the CRC for a given RTUMessage and add it to the end
+void RTUutils::addCRC(ModbusMessage& raw) {
+  uint16_t crc16 = calcCRC(raw.data(), raw.size());
+  raw.push_back(crc16 & 0xff);
+  raw.push_back((crc16 >> 8) & 0xFF);
+}
+
+// calculateInterval: determine the minimal gap time between messages
+uint32_t RTUutils::calculateInterval(HardwareSerial& s, uint32_t overwrite) {
+  uint32_t interval = 0;
+
+  // silent interval is at least 3.5x character time
+  interval = 35000000UL / s.baudRate();  // 3.5 * 10 bits * 1000 µs * 1000 ms / baud
+  if (interval < 1750) interval = 1750;       // lower limit according to Modbus RTU standard
+  // User overwrite?
+  if (overwrite > interval) {
+    interval = overwrite;
+  }
+  return interval;
+}
+
+// UARTinit: modify the UART FIFO copy trigger threshold 
+// This is normally set to 112 by default, resulting in short messages not being 
+// recognized fast enough for higher Modbus bus speeds
+// Our default is 1 - every single byte arriving will have the UART FIFO
+// copied to the serial buffer.
+int RTUutils::UARTinit(HardwareSerial& serial, int thresholdBytes) {
+  int rc = 0;
+#if NEED_UART_PATCH
+  // Is the threshold value valid? The UART FIFO is 128 bytes only
+  if (thresholdBytes > 0 && thresholdBytes < 128) {
+    // Yes, it is. Try to identify the Serial/Serial1/Serial2 the user has provided.
+    // Is it Serial (UART0)?
+    if (&serial == &Serial) {
+      // Yes. get the current value and set ours instead
+      rc = UART0.conf1.rxfifo_full_thrhd;
+      UART0.conf1.rxfifo_full_thrhd = thresholdBytes;
+      LOG_D("Serial FIFO threshold set to %d (was %d)\n", thresholdBytes, rc);
+    // No, but perhaps is Serial1?
+    } else if (&serial == &Serial1) {
+      // It is. Get the current value and set ours.
+      rc = UART1.conf1.rxfifo_full_thrhd;
+      UART1.conf1.rxfifo_full_thrhd = thresholdBytes;
+      LOG_D("Serial1 FIFO threshold set to %d (was %d)\n", thresholdBytes, rc);
+    // No, but it may be Serial2
+    } else if (&serial == &Serial2) {
+      // Found it. Get the current value and set ours.
+      rc = UART2.conf1.rxfifo_full_thrhd;
+      UART2.conf1.rxfifo_full_thrhd = thresholdBytes;
+      LOG_D("Serial2 FIFO threshold set to %d (was %d)\n", thresholdBytes, rc);
+    // None of the three, so we are at an end here
+    } else {
+      LOG_W("Unable to identify serial\n");
+    }
+  } else {
+    Serial.printf("Threshold must be between 1 and 127! (was %d)", thresholdBytes);
+  }
+#endif
+  // Return the previous value in case someone likes to see it.
+  return rc;
+}
+
+// send: send a message via Serial, watching interval times - including CRC!
+void RTUutils::send(HardwareSerial& serial, uint32_t& lastMicros, uint32_t interval, RTScallback rts, const uint8_t *data, uint16_t len) {
+  uint16_t crc16 = calcCRC(data, len);
+
+  // Clear serial buffers
+  while (serial.available()) serial.read();
+
+  // Respect interval
+  if (micros() - lastMicros < interval) delayMicroseconds(interval - (micros() - lastMicros));
+
+  // Toggle rtsPin, if necessary
+  rts(HIGH);
+  // Write message
+  serial.write(data, len);
+  // Write CRC in LSB order
+  serial.write(crc16 & 0xff);
+  serial.write((crc16 >> 8) & 0xFF);
+  serial.flush();
+  // Toggle rtsPin, if necessary
+  rts(LOW);
+
+  HEXDUMP_D("Sent packet", data, len);
+
+  // Mark end-of-message time for next interval
+  lastMicros = micros();
+}
+
+// send: send a message via Serial, watching interval times - including CRC!
+void RTUutils::send(HardwareSerial& serial, uint32_t& lastMicros, uint32_t interval, RTScallback rts, ModbusMessage raw) {
+  send(serial, lastMicros, interval, rts, raw.data(), raw.size());
+}
+
+// receive: get (any) message from Serial, taking care of timeout and interval
+ModbusMessage RTUutils::receive(HardwareSerial& serial, uint32_t timeout, uint32_t& lastMicros, uint32_t interval) {
+  // Allocate initial receive buffer size: 1 block of BUFBLOCKSIZE bytes
+  const uint16_t BUFBLOCKSIZE(512);
+  uint8_t *buffer = new uint8_t[BUFBLOCKSIZE];
+  ModbusMessage rv;
+
+  // Index into buffer
+  register uint16_t bufferPtr = 0;
+  // Byte read
+  register int b; 
+  // Flag for successful read cycle
+  bool hadBytes = false;
+  // Next buffer limit
+
+  // State machine states
+  enum STATES : uint8_t { WAIT_DATA = 0, IN_PACKET, DATA_READ, FINISHED };
+  register STATES state = WAIT_DATA;
+
+  // Timeout tracker
+  uint32_t TimeOut = millis();
+
+  // interval tracker 
+  uint32_t intervalEnd = micros();
+
+  while (state != FINISHED) {
+    switch (state) {
+    // WAIT_DATA: await first data byte, but watch timeout
+    case WAIT_DATA:
+      if (serial.available()) {
+        state = IN_PACKET;
+        intervalEnd = micros();
+		delay(70);  // for DTSU666 new librrary change
+      } else {
+        if (millis() - TimeOut >= timeout) {
+          rv.push_back(TIMEOUT);
+          state = FINISHED;
+        }
+      }
+      delay(1);
+      break;
+    // IN_PACKET: read data until a gap of at least _interval time passed without another byte arriving
+    case IN_PACKET:
+      hadBytes = false;
+      b = serial.read();
+      if (b >= 0) {
+        hadBytes = true;
+      }
+      while (b >= 0) {
+        buffer[bufferPtr++] = b;
+        // Buffer full?
+        if (bufferPtr >= BUFBLOCKSIZE) {
+          // Yes. Something fishy here - bail out!
+          rv.push_back(PACKET_LENGTH_ERROR);
+          state = FINISHED;
+          break;
+        }
+        b = serial.read();
+      }
+      // Did we read some?
+      if (hadBytes) {
+        // Yes, take another turn
+        intervalEnd = micros();
+        delay(1);
+      } else {
+        // No. Has a complete interval passed without data?
+        lastMicros = micros();
+        if (lastMicros - intervalEnd >= interval) {
+          // Yes, go processing data
+          state = DATA_READ;
+        }
+      }
+      break;
+    // DATA_READ: successfully gathered some data. Prepare return object.
+    case DATA_READ:
+      // Did we get a sensible buffer length?
+      HEXDUMP_D("Raw buffer received", buffer, bufferPtr);
+      if (bufferPtr >= 4)
+      {
+        // Yes. Allocate response object
+        for (uint16_t i = 0; i < bufferPtr; ++i) {
+          rv.push_back(buffer[i]);
+        }
+        state = FINISHED;
+      } else {
+        // No, packet was too short for anything usable. Return error
+        rv.push_back(PACKET_LENGTH_ERROR);
+        state = FINISHED;
+      }
+      break;
+    // FINISHED: we are done, clean up.
+    case FINISHED:
+      // CLear serial buffer in case something is left trailing
+      // May happen with servers too slow!
+      while (serial.available()) serial.read();
+      break;
+    }
+  }
+  // Deallocate buffer
+  delete[] buffer;
+
+  HEXDUMP_D("Received packet", rv.data(), rv.size());
+
+  return rv;
+}
